4e85ee570415c4c70c735a6226ed2410c7c31f31
satan <28350180+oceanboi1337@users.noreply.github.com>
2023-05-28 06:04:53 +0200

    ya

---
+++ main.py
@@ -0,0 +1,29 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+
+    return '<h1>Index</h1>'
+
+@app.route('/login', methods=['GET'])
+def get_login():
+
+    return flask.send_file('index.html')
+
+@app.route('/login', methods=['POST'])
+def post_login():
+
+    username = flask.request.form.get('username')
+    password = flask.request.form.get('password')
+
+    if username and password:
+
+        if username == 'admin' and password == '11223344':
+
+            return flask.make_response('<h1>Login Success</h1>')
+        
+    return flask.make_response('<h1>Login failed</h1>', 401)
+
+app.run()
