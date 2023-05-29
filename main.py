1f79b965b170ac587584a45994c35f3bf17cd0f3
satan <28350180+oceanboi1337@users.noreply.github.com>
2023-05-29 06:16:56 +0200

    CSRF Demo

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import flask, logging
+import flask, logging, os
 
 app = flask.Flask(__name__)
+app.secret_key = os.urandom(32)
 
 app.logger.disabled = True
 log = logging.getLogger('werkzeug')
 log.disabled = True
+
+def generate_csrf():
+    return os.urandom(16).hex()
 
 @app.route('/')
 def index():
@@ -14,20 +18,32 @@ def index():
 @app.route('/login', methods=['GET'])
 def get_login():
 
-    return flask.send_file('index.html')
+    csrf = generate_csrf()
+    flask.session['csrf-token'] = csrf
+
+    return flask.render_template('login.html', csrf=csrf)
 
 @app.route('/login', methods=['POST'])
 def post_login():
 
     username = flask.request.form.get('username')
     password = flask.request.form.get('password')
+    post_csrf = flask.request.form.get('csrf-token')
     
-    if username and password:
+    if (session_csrf := flask.session.get('csrf-token')) == None:
+        print('invalid csrf')
+        return flask.make_response('<h1>Login failed</h1>', 401)
 
-        if username == 'admin' and password == '11223344':
+    if not username or not password:
+        return 'No username or password entered'
 
-            return flask.make_response('<h1>Login Success</h1>')
-        
+    if post_csrf != session_csrf:
+        return flask.make_response('<h1>Login failed</h1>', 401)
+
+    if username == 'admin' and password == '11223344':
+        print('login success')
+        return flask.make_response('<h1>Login Success</h1>')
+    
     return flask.make_response('<h1>Login failed</h1>', 401)
 
 app.run(host='0.0.0.0', debug=False, threaded=True)
